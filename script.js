00529c236fb7247f2565e2e76131047833f0a288
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-12-02 18:59:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ const prevButton = document.querySelector("#prev");
 let position = 1;
 
 function next(){
-	if(position<5){
+	if (prevButton.disabled) prevButton.disabled = false;
+	if(position < 5){
 
-       if (prevButton.disabled) prevButton.disabled = false;
-		
 		const nextLine = currentElement.nextElementSibling;
 		nextLine.classList.add("active");
-		const nextCircle = currentElement.nextElementSibling;
+		const nextCircle = nextLine.nextElementSibling;
 		nextCircle.classList.add("active");
 
 		currentElement = nextCircle;
@@ -23,18 +22,18 @@ function next(){
 	}
 }
 
-function prev() {
-	if(position!==1){
 
-      if (nextButton.disabled) nextButton.disabled = false;
-		
-		currentElement.classList.remove("active");
-		 const prevLine = currentElement.previousElementSibling;
-		prevLine.classList.remove("active");
+function prev() {
+    if (nextButton.disabled) nextButton.disabled = false;
+    if (position !== 1) {
+        currentElement.classList.remove("active");
+        const prevLine = currentElement.previousElementSibling;
+        prevLine.classList.remove("active");
         currentElement = prevLine.previousElementSibling;
         position--;
-		if (position === 1) {
+
+        if (position === 1) {
             prevButton.disabled = true;
         }
-	}
+    }
 }
